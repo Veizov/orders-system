7229b869808fdac48db64044bbb7999f6e3c4010
Denislav Veizov <denislavveizov@gmail.com>
2023-08-25 11:06:19 +0300

    Project restructure

--- scripts/down-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker compose -f ./merchant/docker/docker-compose.yml down
-docker compose -f ./notification-provider/docker/docker-compose.yml down
-
--- scripts/up-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker compose -f ./merchant/docker/docker-compose.yml up -d
-docker compose -f ./notification-provider/docker/docker-compose.yml up -d
