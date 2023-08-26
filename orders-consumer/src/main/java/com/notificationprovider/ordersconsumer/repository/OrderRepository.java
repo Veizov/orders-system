60470ef81dddea0408a43d3424061db99df20d6c
Denislav Veizov <denislavveizov@gmail.com>
2023-08-26 23:46:26 +0300

    Create order repository

---
+++ orders-consumer/src/main/java/com/notificationprovider/ordersconsumer/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.notificationprovider.ordersconsumer.repository;
+
+import com.notificationprovider.ordersconsumer.domain.entity.OrderEntity;
+
+public interface OrderRepository extends BaseRepository<OrderEntity, Long> {
+
+}
