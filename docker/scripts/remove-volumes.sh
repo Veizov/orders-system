92bb720e05e94b136d931049821f99df06d4565a
Denislav Veizov <denislavveizov@gmail.com>
2023-08-29 11:41:39 +0300

    Fix problem with remove volumes script

--- docker/scripts/remove-volumes.sh
+++ docker/scripts/remove-volumes.sh
@@ -2,4 +2,4 @@
 # Version: 1.0
 # Author : Denislav Veizov
 
-docker volume list | grep 'orders-system_' | awk '{print $2}'
+docker volume rm $(docker volume list | grep 'orders-system_' | awk '{print $2}')
